03062ea50ae6714d114a16f5160e4fb820228689
Alejandro Bravo <alejandrob@spacebarmedia.com>
2021-04-18 20:10:51 -0500

    Finshed all styling, added date and time below each message and added a few snimations

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { operators } from '../enums/chat-operators';
 import './chat.scss';
 import sendImage from '../img/send.png';
@@ -16,6 +16,8 @@ function Chat() {
     const [botMessage, setBotMessage] = useState('');
     const [chatHidden, setChatHidden] = useState(true);
 
+    const liveChatRef = useRef(null);
+
     const operatorChat = () => {
 		let randResponse = operators.responses[Math.floor(Math.random()*operators.responses.length)];
 		dispatchChatEvent(randResponse, "operator");
@@ -30,18 +32,20 @@ function Chat() {
 	}
 
 	const dispatchChatEvent = (msg, from) => {
-		let event = new CustomEvent('chatreceived', {"detail":{datetime:new Date().toISOString(), message:msg, from:from}});
+		let event = new CustomEvent('chatreceived', {"detail":{datetime:new Date().toLocaleString(), message:msg, from:from}});
 		// Listen for the event
 		addListener('chatreceived', (e) => {
+            let d = new Date();
+            d.setTime(d.getTime()-200000);
             if(e.chat.from === 'visitor'){
-                setNewMessage([...newMessage, {message:inputMessage, from: 'visitor'}]);
+                setNewMessage([...newMessage, {message:inputMessage, from: 'visitor', datetime:new Date(d.setTime(d.getTime()+2000)).toLocaleString()}]);
             }else if(e.chat.from === 'operator'){
                 setBotMessage(e.chat.message);
             }
         }, _events, false);
 
 		// Dispatch the event.
-		raiseEvent("chatreceived", {"chat":{datetime:new Date().toISOString(), message:msg, from:from}}, _events);
+		raiseEvent("chatreceived", {"chat":{datetime:new Date().toLocaleString(), message:msg, from:from}}, _events);
 	}
     const addListener = (eventName, callback, events) => {
 	 	callbacks = events[eventName] = events[eventName] || [];
@@ -59,6 +63,7 @@ function Chat() {
 
     const showChatPanel = () => {
         setChatHidden(!chatHidden);
+        getChatHistory();
     }
     
     const sendChat = () => {
@@ -66,43 +71,45 @@ function Chat() {
         setInputMessage('');
 		dispatchChatEvent(inputMsg, "visitor");
 		if(inputMsg.indexOf("hello") !== -1 || inputMsg.indexOf("hi") !== -1) {
-			setTimeout(operatorGreetingChat, 2000);
+			setTimeout(operatorGreetingChat, 1000);
 		} else if(inputMsg.indexOf("?") !== -1) {
-			setTimeout(operatorAnswerChat, 2000);
+			setTimeout(operatorAnswerChat, 1000);
 		} else {
-			setTimeout(operatorChat, 2000);
+			setTimeout(operatorChat, 1000);
 		}
 	}
     const getChatHistory = () => {
 		let d = new Date();
 		d.setTime(d.getTime()-200000);
         let history = []
-		history.push({datetime:new Date(d.setTime(d.getTime()+2000)).toISOString(), message:"hello", from:"visitor"});
-		history.push({datetime:new Date(d.setTime(d.getTime()+4000)).toISOString(), message:"Hi, how can I help you?", from:"operator"});
-		history.push({datetime:new Date(d.setTime(d.getTime()+4000)).toISOString(), message:"I'm looking for a size 7, but can't find it", from:"visitor"});
-		history.push({datetime:new Date(d.setTime(d.getTime()+4000)).toISOString(), message:"Ok, one moment I'll check the stock", from:"operator"})
-		history.push({datetime:new Date(d.setTime(d.getTime()+10000)).toISOString(), message:"I'm sorry, there is no sie 7 available in that colour. There are some in red and blue however", from:"operator"})
-		history.push({datetime:new Date(d.setTime(d.getTime()+4000)).toISOString(), message:"Oh great, thank you", from:"visitor"});
-		history.push({datetime:new Date(d.setTime(d.getTime()+4000)).toISOString(), message:"my pleasure :-)", from:"operator"});
+		history.push({datetime:new Date(d.setTime(d.getTime()+2000)).toLocaleString(), message:"hello", from:"visitor"});
+		history.push({datetime:new Date(d.setTime(d.getTime()+4000)).toLocaleString(), message:"Hi, how can I help you?", from:"operator"});
+		history.push({datetime:new Date(d.setTime(d.getTime()+4000)).toLocaleString(), message:"I'm looking for a size 7, but can't find it", from:"visitor"});
+		history.push({datetime:new Date(d.setTime(d.getTime()+4000)).toLocaleString(), message:"Ok, one moment I'll check the stock", from:"operator"})
+		history.push({datetime:new Date(d.setTime(d.getTime()+10000)).toLocaleString(), message:"I'm sorry, there is no sie 7 available in that colour. There are some in red and blue however", from:"operator"})
+		history.push({datetime:new Date(d.setTime(d.getTime()+4000)).toLocaleString(), message:"Oh great, thank you", from:"visitor"});
+		history.push({datetime:new Date(d.setTime(d.getTime()+4000)).toLocaleString(), message:"my pleasure :-)", from:"operator"});
 			
         setTimeout(() => {
             setChats([...history]);
         }, 1000);
 	}
-
-    useEffect(() => {
-        getChatHistory();
-    }, [])
     
     useEffect(() => {
         setNewMessage((oldArray) => {
             if(oldArray === newMessage){
-               return [...oldArray, {message: botMessage, from: 'operator'}];
+                let d = new Date();
+                d.setTime(d.getTime()-200000);
+               return [...oldArray, {message: botMessage, from: 'operator', datetime:new Date(d.setTime(d.getTime()+2000)).toLocaleString()}];
             }else{
                 return [...oldArray];
             }
         });
     }, [botMessage])
+
+    useEffect(() => {
+        liveChatRef.current.scrollTop = liveChatRef.current.scrollHeight;
+    }, [newMessage])
 
 
     return (
@@ -116,13 +123,23 @@ function Chat() {
             <div id="chatHolder">
                 <div id="chatHistory">
                     {chats.map((chatLine, i) => {
-                        return <p key={i}> {chatLine.message} </p>
+                        return (
+                            <>
+                                <p key={i+'a'} className={chatLine.from}> {chatLine.message} </p>
+                                <span key={i+'b'} className="date-history">{chatLine.datetime}</span>
+                            </>
+                        )
                     })}
                 </div>
-                <div id="liveChat">
+                <div id="liveChat" ref={liveChatRef}>
                     {newMessage.map((liveChatLine, i) => {
                         if(liveChatLine.message !== null && liveChatLine.message !== ''){
-                            return <div className={`container-${liveChatLine.from}`}><p key={i} className={liveChatLine.from}> {liveChatLine.message} </p></div>
+                            return (
+                                <>
+                                    <div className={`container-${liveChatLine.from}`} key={i+'a'}><p key={i+'b'} className={liveChatLine.from}> {liveChatLine.message} </p></div>
+                                    <span className="date-live" key={i+'c'}>{liveChatLine.datetime}</span>
+                                </>
+                            )
                         }else{
                             return false;
                         }
@@ -130,9 +147,11 @@ function Chat() {
                 </div>
                 
             </div>
-            <textarea id="chatInput" placeholder='Type your question here...' onChange={(event) => {setInputMessage(event.target.value)}} value={inputMessage}></textarea>
-            <div id="chatSubmit">
-                <img className='sendImage' src={sendImage} onClick={sendChat} />
+            <div className="input-container">
+                <textarea id="chatInput" placeholder='Type your question here...' onChange={(event) => {setInputMessage(event.target.value)}} value={inputMessage}></textarea>
+                <div id="chatSubmit">
+                    <img className='sendImage' src={sendImage} onClick={sendChat} />
+                </div>
             </div>
         </div>
         </>
